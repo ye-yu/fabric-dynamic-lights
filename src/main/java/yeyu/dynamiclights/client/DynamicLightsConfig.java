72d275d11d674436d5df0d2186213a7e63942e0c
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2021-06-20 01:42:45 +0800

    Add light on fire and enchantment

--- src/main/java/yeyu/dynamiclights/client/DynamicLightsConfig.java
+++ src/main/java/yeyu/dynamiclights/client/DynamicLightsConfig.java
@@ -4,14 +4,19 @@ import com.google.common.collect.Maps;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -27,6 +32,9 @@ public enum DynamicLightsConfig implements Consumer<NbtCompound> {
         public final Defaults.Boolean LIGHT_STRENGTH_BY_ITEM = new Defaults.Boolean("light strength by item", false);
         public final Defaults.Int LIGHT_STRENGTH_INT = new Defaults.Int("light strength level", 0);
         public final Defaults.Float LIGHT_SOURCE_OFFSET = new Defaults.Float("light source offset", 1f);
+        public final Defaults.Int LIGHT_ENCHANTMENT_INT = new Defaults.Int("enchantment light level", 5);
+        public final Defaults.Int LIGHT_FIRE_INT = new Defaults.Int("fire light level", 12);
+
         @Override
         public void accept(NbtCompound nbtCompound) {
             final String id = ID.get(nbtCompound);
@@ -36,23 +44,48 @@ public enum DynamicLightsConfig implements Consumer<NbtCompound> {
             final Boolean overwrite = OVERWRITE.get(nbtCompound);
             final Float lightSourceOffSet = LIGHT_SOURCE_OFFSET.get(nbtCompound);
             final Integer lightStrengthInt = LIGHT_STRENGTH_INT.get(nbtCompound);
+            final Integer lightEnchantmentInt = LIGHT_ENCHANTMENT_INT.get(nbtCompound);
+            final Integer lightFireInt = LIGHT_FIRE_INT.get(nbtCompound);
 
             if (overwrite) {
-                DynamicLightsManager.INSTANCE.registerEntityTick(Identifier.tryParse(id), getEntityClientWorldBiConsumer(lightStrengthByItem, lightSourceOffSet, lightStrengthInt));
+                DynamicLightsManager.INSTANCE.registerEntityTick(Identifier.tryParse(id), getEntityClientWorldBiConsumer(lightStrengthByItem, lightSourceOffSet, lightStrengthInt, lightEnchantmentInt, lightFireInt));
             } else {
-                DynamicLightsManager.INSTANCE.appendEntityTick(Identifier.tryParse(id), getEntityClientWorldBiConsumer(lightStrengthByItem, lightSourceOffSet, lightStrengthInt));
+                DynamicLightsManager.INSTANCE.appendEntityTick(Identifier.tryParse(id), getEntityClientWorldBiConsumer(lightStrengthByItem, lightSourceOffSet, lightStrengthInt, lightEnchantmentInt, lightFireInt));
             }
         }
 
         @NotNull
-        private BiConsumer<Entity, ClientWorld> getEntityClientWorldBiConsumer(Boolean lightStrengthByItem, Float lightSourceOffSet, Integer lightStrengthInt) {
+        private BiConsumer<Entity, ClientWorld> getEntityClientWorldBiConsumer(Boolean lightStrengthByItem, Float lightSourceOffSet, Integer lightStrengthInt, Integer lightEnchantmentInt, Integer lightFireInt) {
             return (entity, clientWorld) -> {
                 if (lightStrengthByItem) {
-                    DynamicLightsUtils.handleEntityLightsByItem(entity, clientWorld, lightSourceOffSet);
-                } else if (lightStrengthInt > 0) {
-                    DynamicLightsUtils.handleEntity(entity, clientWorld, lightStrengthInt);
+                    DynamicLightsUtils.handleEntityLightsByItem(entity, clientWorld, lightSourceOffSet, lightEnchantmentInt, lightFireInt);
+                } else {
+                    DynamicLightsUtils.handleEntity(entity, clientWorld, lightStrengthInt, lightEnchantmentInt, lightFireInt);
                 }
             };
+        }
+    },
+    ITEM("item") {
+        public final Defaults.String ID = new Defaults.String("id", null);
+        public final Defaults.Int LIGHT_STRENGTH_INT = new Defaults.Int("light strength level", 0);
+        public final Defaults.Int LIGHT_ENCHANTMENT_INT = new Defaults.Int("enchantment light level", 0);
+        public final Defaults.Int LIGHT_FIRE_INT = new Defaults.Int("fire light level", 12);
+
+        @Override
+        public void accept(NbtCompound nbtCompound) {
+            final String id = ID.get(nbtCompound);
+            if (id == null) throw new RuntimeException("key 'id' has no value");
+            final Integer lightStrengthInt = LIGHT_STRENGTH_INT.get(nbtCompound);
+            final Integer lightEnchantmentInt = LIGHT_ENCHANTMENT_INT.get(nbtCompound);
+            final Integer lightFireInt = LIGHT_FIRE_INT.get(nbtCompound);
+            final Item matchedItem = Registry.ITEM.get(Identifier.tryParse(id));
+
+            DynamicLightsManager.INSTANCE.appendEntityTick(EntityType.ITEM, (entity, clientWorld) -> {
+                final Item item = entity.getStack().getItem();
+                if (item != matchedItem) return;
+                DynamicLightsUtils.handleEntity(entity, clientWorld, lightStrengthInt, lightEnchantmentInt, lightFireInt);
+            });
+
         }
     };
 
@@ -70,6 +103,15 @@ public enum DynamicLightsConfig implements Consumer<NbtCompound> {
             REVERSE_MAP = Maps.newHashMap();
             Arrays.stream(DynamicLightsConfig.values()).forEach(e -> REVERSE_MAP.put(e.string, e));
         }
+        DynamicLightsManager.INSTANCE.clear();
+        for (EntityType<?> entityType : Registry.ENTITY_TYPE) {
+            if (entityType == EntityType.ITEM) continue;
+            final Identifier id = Registry.ENTITY_TYPE.getId(entityType);
+            final NbtCompound nbtCompound = new NbtCompound();
+            nbtCompound.putString("id", id.toString());
+            DynamicLightsConfig.ENTITY.accept(nbtCompound);
+            LOGGER.info("Loaded default: {}", id);
+        }
 
         // bootstrap resources directory
         {
@@ -84,12 +126,26 @@ public enum DynamicLightsConfig implements Consumer<NbtCompound> {
         // bootstrap run directory
         final File runDirectory = MinecraftClient.getInstance().runDirectory;
         final File targetDirectory = new File(runDirectory, "config/dynamic-lights");
+        if (!targetDirectory.exists()) {
+            //noinspection ResultOfMethodCallIgnored
+            targetDirectory.mkdirs();
+        }
         final File[] files = targetDirectory.listFiles();
         if (files == null) return;
-        DynamicLightsManager.INSTANCE.clear();
+        for (File file : files) {
+            try {
+                final FileInputStream fileInputStream = new FileInputStream(file);
+                parse(fileInputStream, () -> "Custom file: " + file.toString());
+
+            } catch (FileNotFoundException e) {
+                LOGGER.error("Error in parsing {}", file.toString());
+                LOGGER.trace(e);
+            }
+        }
     }
 
     public static void parse(@Nullable InputStream inputStream, Supplier<String> nameSupplier) {
+        LOGGER.info("Loading {}", nameSupplier.get());
         if (inputStream == null) return;
         final Scanner scanner = new Scanner(inputStream);
         NbtCompound nbtCompound = new NbtCompound();
--- src/main/java/yeyu/dynamiclights/client/DynamicLightsUtils.java
+++ src/main/java/yeyu/dynamiclights/client/DynamicLightsUtils.java
@@ -2,37 +2,64 @@ package yeyu.dynamiclights.client;
 
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.InventoryOwner;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
 
 public class DynamicLightsUtils {
-    public static void handleEntity(Entity entity, ClientWorld clientWorld, int fixedLightLevel) {
-        final Vec3d camera = entity.getCameraPosVec(1);
-        Vec3d rotationVec = entity.getRotationVec(1);
-        Vec3d cameraPosVec = camera.add(rotationVec.x * 1.5, rotationVec.y * 1.5, rotationVec.z * 1.5);
-        final float animationFactor = DynamicLightsStorage.animationFactor(entity, (entity instanceof LivingEntity) &&  ((LivingEntity)entity).isDead() ? 0 : fixedLightLevel);
-        DynamicLightsOption.getCurrentOption().iterateLightMap(cameraPosVec, clientWorld, animationFactor);
+    public static void handleEntity(Entity entity, ClientWorld clientWorld, int fixedLightLevel, Integer lightEnchantmentInt, Integer lightFireInt) {
+        Vec3d cameraPosVec = entity.getCameraPosVec(1);
+        fixedLightLevel = Math.max(fixedLightLevel, hasEnchantment(entity) ? lightEnchantmentInt : 0);
+        fixedLightLevel = Math.max(fixedLightLevel, entity.isOnFire() ? lightFireInt : 0);
+        float maxLight = DynamicLightsStorage.animationFactor(entity, (entity instanceof LivingEntity) &&  ((LivingEntity)entity).isDead() ? 0 : fixedLightLevel);
+        if (maxLight > 0) DynamicLightsOption.getCurrentOption().iterateLightMap(cameraPosVec, clientWorld, maxLight);
     }
 
-    public static void handleEntityLightsByItem(Entity entity, ClientWorld clientWorld, float offset) {
+    public static void handleEntityLightsByItem(Entity entity, ClientWorld clientWorld, float offset, Integer lightEnchantmentInt, Integer lightFireInt) {
         final Vec3d camera = entity.getCameraPosVec(1);
         Vec3d rotationVec = entity.getRotationVec(1);
         Vec3d cameraPosVec = camera.add(rotationVec.x * offset, rotationVec.y * offset, rotationVec.z * offset);
-        final int maxLight = entity.isSpectator() ? 8 : getItemLightLevel(entity);
-        final float animationFactor = DynamicLightsStorage.animationFactor(entity, maxLight);
-        DynamicLightsOption.getCurrentOption().iterateLightMap(cameraPosVec, clientWorld, animationFactor);
+        int itemLightLevel = entity.isSpectator() ? 8 : getItemLightLevel(entity);
+        itemLightLevel = Math.max(itemLightLevel, hasEnchantment(entity) ? lightEnchantmentInt : 0);
+        itemLightLevel = Math.max(itemLightLevel, entity.isOnFire() ? lightFireInt : 0);
+        float maxLight = DynamicLightsStorage.animationFactor(entity, itemLightLevel);
+        if (maxLight > 0) DynamicLightsOption.getCurrentOption().iterateLightMap(cameraPosVec, clientWorld, maxLight);
     }
 
     private static int getItemLightLevel(Entity entity) {
-        if ((entity instanceof LivingEntity)) {
-            final Item mainHandItem = ((LivingEntity) entity).getMainHandStack().getItem();
-            final Item offHandItem = ((LivingEntity) entity).getOffHandStack().getItem();
-            final Integer mainHandItemLightLevel = DynamicLightsClient.ITEM_BLOCK_LIGHT_LEVEL.getOrDefault(mainHandItem, 0);
-            final Integer offHandItemLightLevel = DynamicLightsClient.ITEM_BLOCK_LIGHT_LEVEL.getOrDefault(offHandItem, 0);
-            return Math.max(mainHandItemLightLevel, offHandItemLightLevel);
-        }
+        if (!(entity instanceof LivingEntity)) return 0;
+        final Item mainHandItem = ((LivingEntity) entity).getMainHandStack().getItem();
+        final Item offHandItem = ((LivingEntity) entity).getOffHandStack().getItem();
+        final Integer mainHandItemLightLevel = DynamicLightsClient.ITEM_BLOCK_LIGHT_LEVEL.getOrDefault(mainHandItem, 0);
+        final Integer offHandItemLightLevel = DynamicLightsClient.ITEM_BLOCK_LIGHT_LEVEL.getOrDefault(offHandItem, 0);
+        return Math.max(mainHandItemLightLevel, offHandItemLightLevel);
+    }
 
-        return 0;
+    private static boolean hasEnchantment(Entity entity) {
+        if (entity instanceof InventoryOwner) {
+            final Inventory inventory = ((InventoryOwner) entity).getInventory();
+            for (int i = 0; i < inventory.size(); i++) {
+                final ItemStack stack = inventory.getStack(i);
+                if (stack.hasEnchantments()) return true;
+            }
+            return false;
+        }
+        if (entity instanceof PlayerEntity) {
+            final Inventory inventory = ((PlayerEntity) entity).getInventory();
+            for (int i = 0; i < inventory.size(); i++) {
+                final ItemStack stack = inventory.getStack(i);
+                if (stack.hasEnchantments()) return true;
+            }
+            return false;
+        }
+        if (entity instanceof ItemEntity) {
+            return ((ItemEntity) entity).getStack().hasEnchantments();
+        }
+        return false;
     }
 }
