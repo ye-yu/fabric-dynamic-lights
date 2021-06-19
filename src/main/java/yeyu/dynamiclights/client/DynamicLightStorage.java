665c11db1e533718a8b23cc8b326ad2cba7516b6
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2021-06-19 21:08:55 +0800

    Add disable dynamic light option

--- src/main/java/yeyu/dynamiclights/client/DynamicLightStorage.java
+++ src/main/java/yeyu/dynamiclights/client/DynamicLightStorage.java
@@ -1,6 +1,7 @@
 package yeyu.dynamiclights.client;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,8 +9,13 @@ import java.util.Map;
 public class DynamicLightStorage {
     public static Map<Long, Double> BP_TO_LIGHT_LEVEL = new HashMap<>();
 
-    public static boolean setLightLevel(BlockPos bp, double lightLevel) {
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    public static boolean setLightLevel(BlockPos bp, double lightLevel, boolean force) {
         final long bpLong = bp.asLong();
+        if (force) {
+            final Double previous = BP_TO_LIGHT_LEVEL.put(bpLong, lightLevel);
+            return previous != null && !MathHelper.approximatelyEquals(previous, lightLevel);
+        }
         if (lightLevel < 1e-5) {
             return BP_TO_LIGHT_LEVEL.remove(bpLong) != null;
         } else {
--- src/main/java/yeyu/dynamiclights/client/DynamicLightsOption.java
+++ src/main/java/yeyu/dynamiclights/client/DynamicLightsOption.java
@@ -8,7 +8,7 @@ import net.minecraft.util.math.Vec3d;
 
 public class DynamicLightsOption {
     public static final String OPTION_NAME = "dynamic_light_level";
-    private static DynamicLightsLevel currentOption = DynamicLightsLevel.FOUR;
+    private static DynamicLightsLevel currentOption = DynamicLightsLevel.THREE;
     public static final DoubleOption DYNAMIC_LIGHTS_OPTION = new DoubleOption("dynamiclights.level",
             0.0D,
             DynamicLightsLevel.values().length - 1,
@@ -16,7 +16,7 @@ public class DynamicLightsOption {
             $ -> (double) currentOption.ordinal(),
             ($, mipmapLevels) -> setCurrentOption((int) (double) mipmapLevels),
             ($, option) -> {
-                int d = getCurrentOption().ordinal() + 1;
+                int d = getCurrentOption().ordinal();
                 return new TranslatableText("options.generic_value", new TranslatableText("dynamiclights.level"), d);
             },
             (client) -> client.textRenderer.wrapLines(new TranslatableText("dynamiclights.level.desc"), 200));
@@ -33,17 +33,14 @@ public class DynamicLightsOption {
         return currentOption.MULTIPLIER;
     }
 
-    public static int getCurrentLightRadius() {
-        return currentOption.RADIUS;
-    }
-
     public enum DynamicLightsLevel {
-        TWO(5, 5),
-        THREE(6, 2.5f),
-        FOUR(7, 1.3f),
-        FIVE(8, 0.8f),
-        SIX(9, 0.55f),
-        SEVEN(10, 0.43f);
+        OFF(5, 0),
+        ONE(5, 5),
+        TWO(6, 2.5f),
+        THREE(7, 1.3f),
+        FOUR(8, 0.8f),
+        FIVE(9, 0.55f),
+        SIX(10, 0.43f);
 
         public final int RADIUS;
         public final float MULTIPLIER;
@@ -58,19 +55,23 @@ public class DynamicLightsOption {
             final int radius = RADIUS;
             clientWorld.getProfiler().push("queueCheckLight");
             BlockPos.iterate(playerBp.add(-radius - 1, -radius - 1, -radius - 1), playerBp.add(radius, radius, radius)).forEach((bp) -> {
-                final int vanillaLuminance = clientWorld.getBlockState(bp).getLuminance();
-                final float x = bp.getX() + 0.5f;
-                final float y = bp.getY() + 0.5f;
-                final float z = bp.getZ() + 0.5f;
-                final float camX = (float) cameraPosVec.getX();
-                final float camY = (float) cameraPosVec.getY();
-                final float camZ = (float) cameraPosVec.getZ();
-                final float dx = camX - x;
-                final float dy = camY - y;
-                final float dz = camZ - z;
-                final float dist = dx * dx + dy * dy + dz * dz;
-                final double lightLevel = Math.max(LightFunction.QUADRATIC.apply(dist, maxLight), vanillaLuminance);
-                if (!DynamicLightStorage.setLightLevel(bp, lightLevel)) return;
+                if (this == DynamicLightsLevel.OFF) {
+                    if (!DynamicLightStorage.setLightLevel(bp, 0, false)) return;
+                } else {
+                    final int vanillaLuminance = clientWorld.getBlockState(bp).getLuminance();
+                    final float x = bp.getX() + 0.5f;
+                    final float y = bp.getY() + 0.5f;
+                    final float z = bp.getZ() + 0.5f;
+                    final float camX = (float) cameraPosVec.getX();
+                    final float camY = (float) cameraPosVec.getY();
+                    final float camZ = (float) cameraPosVec.getZ();
+                    final float dx = camX - x;
+                    final float dy = camY - y;
+                    final float dz = camZ - z;
+                    final float dist = dx * dx + dy * dy + dz * dz;
+                    final double lightLevel = Math.max(LightFunction.QUADRATIC.apply(dist, maxLight), vanillaLuminance);
+                    if (!DynamicLightStorage.setLightLevel(bp, lightLevel, false)) return;
+                }
                 clientWorld.getChunkManager().getLightingProvider().checkBlock(bp);
             });
             clientWorld.getProfiler().pop();
