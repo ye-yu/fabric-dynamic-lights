5a3b13ab0c03065521d29375a115cc94f6976efd
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2021-06-20 15:40:37 +0800

    Refactor codes;

--- src/main/java/yeyu/dynamiclights/client/DynamicLightsOptions.java
+++ src/main/java/yeyu/dynamiclights/client/DynamicLightsOptions.java
@@ -8,7 +8,6 @@ import net.minecraft.util.Pair;
 public class DynamicLightsOptions {
     private static DynamicLightsLevel currentOption = DynamicLightsLevel.THREE;
     private static int maxEntitiesTick = 4;
-
     public static final Pair<String, Option> DYNAMIC_LIGHTS_OPTIONS = new Pair<>("dynamiclights.level", (new DoubleOption("dynamiclights.level",
             0.0D,
             DynamicLightsLevel.values().length - 1,
@@ -20,7 +19,6 @@ public class DynamicLightsOptions {
                 return new TranslatableText("options.generic_value", new TranslatableText("dynamiclights.level"), d);
             },
             (client) -> client.textRenderer.wrapLines(new TranslatableText("dynamiclights.level.desc"), 200))));
-
     public static final Pair<String, Option> DYNAMIC_LIGHTS_ENTITIES = new Pair<>("dynamiclights.entities_tick",
             new DoubleOption("dynamiclights.entities_tick",
                     4,
@@ -33,19 +31,10 @@ public class DynamicLightsOptions {
                         return new TranslatableText("options.generic_value", new TranslatableText("dynamiclights.entities_tick"), d);
                     },
                     (client) -> client.textRenderer.wrapLines(new TranslatableText("dynamiclights.entities_tick.desc"), 200)));
-
     public static final Option[] OPTIONS = new Option[]{
             DYNAMIC_LIGHTS_ENTITIES.getRight(),
             DYNAMIC_LIGHTS_OPTIONS.getRight()
     };
-    
-    public final String optionName;
-    public final Option optionUI;
-    
-    DynamicLightsOptions(String optionName, Option optionUI) {
-        this.optionName = optionName;
-        this.optionUI = optionUI;
-    }
 
     public static DynamicLightsLevel getCurrentOption() {
         return currentOption;
@@ -63,11 +52,11 @@ public class DynamicLightsOptions {
         return currentOption.POWER;
     }
 
-    public static void setMaxEntitiesToTick(int max) {
-        maxEntitiesTick = max;
-    }
     public static int getMaxEntitiesToTick() {
         return maxEntitiesTick;
     }
 
+    public static void setMaxEntitiesToTick(int max) {
+        maxEntitiesTick = max;
+    }
 }
--- src/main/java/yeyu/dynamiclights/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/yeyu/dynamiclights/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,11 +1,9 @@
 package yeyu.dynamiclights.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.s2c.play.ItemPickupAnimationS2CPacket;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
